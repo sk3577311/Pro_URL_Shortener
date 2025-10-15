64a45e3bd83c9ff80779abd5e3366d41eba2335c
sameer <sk33577311@gmail>
2025-10-16 01:17:11 +0530

    last commit 15/10/2025 v10

--- app/auth.py
+++ app/auth.py
@@ -4,7 +4,6 @@ import secrets
 from authlib.integrations.starlette_client import OAuth
 from starlette.config import Config
 from starlette.responses import RedirectResponse
-from starlette.middleware.sessions import SessionMiddleware
 from fastapi import APIRouter, Request
 from app.redis_client import redis_client  # optional for session store
 
